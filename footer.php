9d5469384e39ac2f8ac13a1f70c710971d9a9606
Bas Grolleman <bgrolleman@emendo-it.nl>
2017-12-19 08:21:39 +0100

    Update footer

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 		<div id="footer">
-			<h6>Allboutmusic.eu - Copyright 2010 - Contact <a href="mailto:info@allboutmusic.eu">info@allboutmusic.eu</a></h6>
+			<h6>Jtraine.com - Copyright 2017 - Contact <a href="mailto:jtraine73@gmail.com">jtraine73@gmail.com</a></h6>
 		</div>
 		<?php wp_footer() ?>
 	</body>
